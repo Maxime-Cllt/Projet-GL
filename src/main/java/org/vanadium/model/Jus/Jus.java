7717061d6d969ef8dd10c6954f910b42c2c7570e
JuliePrigent <julie.prigent03@gmail.com>
2023-10-03 14:21:15 +0200

    Add Jus javadoc

--- src/main/java/org/vanadium/model/Jus/Jus.java
+++ src/main/java/org/vanadium/model/Jus/Jus.java
@@ -21,42 +21,78 @@ import org.vanadium.model.panier.PanierVideException;
  */
 public class Jus extends Observable implements ContenantFruit{
     private HashMap<Fruit, Double> fruits;
-    
+        /**
+     * @brief Constructeur de la classe Jus qui permet d'initialiser les attributs 
+     */
     public Jus(){
         fruits = new HashMap<>();
     }
     
+        /**
+     * @return String
+     * @brief Méthode toString de la classe jus qui permet d'afficher le contenu du jus
+     */
     @Override
     public String toString() {
         return "Jus{" +
                 "fruits=" + fruits +
                 '}';
     }
+    
+    
+    /**
+     * @return HashMap<Fruit, Double>
+     * @brief Méthode qui permet de récupérer les fruits du jus
+     */
     @Override
     public HashMap<Fruit, Double> getFruits() {
         return fruits;
     }
 
+        /**
+     * @param fruits
+     * @brief Méthode qui permet de modifier les fruits du jus
+     */
     @Override
     public void setFruits(HashMap<Fruit, Double> fruits) {
         this.fruits = fruits;
     }
 
+    
+    /**
+     * @return int
+     * @brief Méthode qui permet de récupérer la taille du jus 
+     */
     @Override
     public int getTailleContenant() {
         return fruits.size();
     }
 
+    /**
+     * @param i
+     * @return Fruit
+     * @brief Méthode qui permet de récupérer un fruit du jus
+     */
     @Override
     public Fruit getFruit(int i) {
         return (Fruit) fruits.keySet().toArray()[i];
     }
     
+     /**
+     * @return boolean
+     * @brief Méthode qui permet de savoir si le jus est vide
+     */
     @Override
     public boolean estVide() {
         return fruits.isEmpty();
     }
 
+    
+    /**
+     * @param fruitQuantity
+     * @throws PanierPleinException
+     * @brief Méthode qui permet d'ajouter un fruit dans le jus
+     */
     @Override
     public void ajout(Map.Entry<Fruit, Double> fruitQuantity) {
         if (fruits.containsKey(fruitQuantity.getKey())) {
@@ -66,6 +102,10 @@ public class Jus extends Observable implements ContenantFruit{
         notifyObservers(this);
     }
 
+   /**
+     * @throws PanierVideException
+     * @brief Méthode qui permet de retirer un fruit du jus
+     */
     @Override
     public void retrait() {
         if (fruits.isEmpty()) {
@@ -76,6 +116,21 @@ public class Jus extends Observable implements ContenantFruit{
         notifyObservers(this);
     }
 
+     /**
+     * @param o
+     * @brief Méthode qui permet de retirer un fruit du jus
+     */
+    @Override
+    public void retrait(Fruit o) {
+        fruits.remove(o);
+        setChanged();
+        notifyObservers(this);
+    }
+    
+     /**
+     * @return double
+     * @brief Méthode qui permet de calculer le prix du jus
+     */
     @Override
     public double getPrixTotal() {
         double prix = 0;
@@ -86,6 +141,10 @@ public class Jus extends Observable implements ContenantFruit{
 
     }
 
+     /**
+     * @param origine
+     * @brief Méthode qui permet de boycotter un fruit
+     */
     @Override
     public void boycotteOrigine(Fruit.Pays origine) {
         for (Map.Entry<Fruit, Double> fruitQuantity : fruits.entrySet()) {
@@ -94,14 +153,12 @@ public class Jus extends Observable implements ContenantFruit{
             }
         }
     }
-
-    @Override
-    public void retrait(Fruit o) {
-        fruits.remove(o);
-        setChanged();
-        notifyObservers(this);
-    }
     
+     /**
+     * @param o
+     * @return boolean
+     * @brief Méthode qui permet de comparer deux jus de fruit
+     */
     @Override
     public boolean equals(Object o) {
         if (o != null && getClass() == o.getClass()) {
