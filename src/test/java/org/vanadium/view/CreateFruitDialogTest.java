8df2119c7b988f6fb21868384f1679bd4b144b15
Rahman Yilmaz <85690773+Sudo-Rahman@users.noreply.github.com>
2023-10-10 11:09:11 +0200

    add test

--- src/test/java/org/vanadium/view/CreateFruitDialogTest.java
+++ src/test/java/org/vanadium/view/CreateFruitDialogTest.java
@@ -3,17 +3,31 @@ package org.vanadium.view;
 import org.junit.jupiter.api.Test;
 import org.vanadium.TestUtils;
 import org.vanadium.interfaces.Fruit;
+import org.vanadium.model.fruit.Pomme;
 
 import javax.swing.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CreateFruitDialogTest {
     @Test
     void addFruit() {
         CreateFruitDialog dialog = new CreateFruitDialog();
+        JComboBox pays = (JComboBox) TestUtils.getChildNamed(dialog, "Pays");
+        pays.setSelectedItem(Fruit.Pays.PORTUGAL);
+        JComboBox type = (JComboBox) TestUtils.getChildNamed(dialog, "Type");
+        type.setSelectedItem(Fruit.Type.POMME);
+        JSpinner prix = (JSpinner) TestUtils.getChildNamed(dialog, "Prix");
+        prix.setValue(2.0);
+        JSpinner quantity = (JSpinner) TestUtils.getChildNamed(dialog, "Quantité");
+        quantity.setValue(2.0);
+
+
         JButton button = (JButton) TestUtils.getChildNamed(dialog, "Ajouter");
         button.doClick();
-        assertTrue(dialog.getFruitItem().getKey() instanceof Fruit);
+        assertTrue(dialog.getFruitItem().getKey() instanceof Pomme);
+        assertSame(dialog.getFruitItem().getKey().getOrigine(), Fruit.Pays.PORTUGAL);
+        assertEquals(2.0, dialog.getFruitItem().getKey().getPrix());
+        assertEquals(2.0, dialog.getFruitItem().getValue());
     }
 }
